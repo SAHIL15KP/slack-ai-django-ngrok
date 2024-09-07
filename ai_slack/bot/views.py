90a4df196278964d164423642408319a5f66df23
SAHIL15KP <sahil15kp@gmail.com>
2024-09-07 12:32:43 +0530

    env added

--- ai_slack/bot/views.py
+++ ai_slack/bot/views.py
@@ -15,12 +15,21 @@ def slack_events_endpoint(request):
         pass
 
     data_type = json_data.get('type')
-    if data_type != "url_verification":
-        return HttpResponse("not allowed ", status = 400)
 
-    challenge = json_data.get('challenge')
-    if challenge is None:
-        return HttpResponse("not allowed ", status=400)
+    print(data_type, json_data)
+    allowed_data_type = [
+        'url-verification',
+        'event_callback'
+    ]
 
-    return HttpResponse(challenge, status=200)
+    if data_type not in allowed_data_type:
+        return HttpResponse("not allowed ", status = 400)  
 
+    if data_type == "url_verification":
+        challenge = json_data.get('challenge')
+        if challenge is None:
+            return HttpResponse("not allowed ", status=400)
+        return HttpResponse(challenge, status=200)
+    
+    return HttpResponse("SUCCESS", status=200)
+    
