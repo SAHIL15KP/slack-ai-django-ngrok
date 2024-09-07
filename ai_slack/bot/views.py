136d7a160d43bd634e06dca41a31ca3cbc5cd4da
SAHIL15KP <sahil15kp@gmail.com>
2024-09-07 16:59:38 +0530

    request from slack

--- ai_slack/bot/views.py
+++ ai_slack/bot/views.py
@@ -3,7 +3,26 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt 
 from django.views.decorators.http import require_POST 
 # Create your views here.
+import helpers
+import requests
+SLACK_BOT_OAUTH_TOKEN = helpers.config('slack_bot_oauth_token',default=None , cast=str)
 
+def send_message(message , channel_id=None):
+    url = "https://slack.com/api/chat.postMessage"
+    headers = {
+    "Content-Type": "application/json; charset=utf-8",
+    "Authorization": f"Bearer {SLACK_BOT_OAUTH_TOKEN}",
+    "Accept":"application/json"
+    }
+    data = {
+    "channel": f"{channel_id}",
+    "text": message
+}
+    # {
+    # "channel": "YOUR_CHANNEL_ID",
+    # "text": "Hello world :tada:"
+    # }
+    return requests.post(url , json=data , headers=headers)
 
 @csrf_exempt
 @require_POST
@@ -32,4 +51,3 @@ def slack_events_endpoint(request):
         return HttpResponse(challenge, status=200)
     
     return HttpResponse("SUCCESS", status=200)
-    
