8f079bc855b0f92fa17dad37479219dc846ce256
SAHIL15KP <sahil15kp@gmail.com>
2024-09-10 21:21:26 +0530

    ffffff

--- ai_slack/ai_slack/settings.py
+++ ai_slack/ai_slack/settings.py
@@ -26,8 +26,7 @@ DEBUG = True
 # settings.py
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.ngrok-free.app']
-CSRF_TRUSTED_ORIGINS = ['https://ebb9-223-178-211-225.ngrok-free.app']
-
+CSRF_TRUSTED_ORIGINS = ['https://1272-223-178-211-29.ngrok-free.app']
 # Application definition
 
 INSTALLED_APPS = [
